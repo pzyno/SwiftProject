eca3e820c52493aa43c5001da53064bd9660a83b
pengzhiyao <905112786@qq.com>
2021-05-23 23:12:06 +0800

    导入项目

---
+++ SwiftProject/Shared/SwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProjectApp.swift
+//  Shared
+//
+//  Created by 彭之耀 on 2021/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
